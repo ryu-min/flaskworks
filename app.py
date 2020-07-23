398f5dcbaa7ff4624602759d4208755fbff2e035
ryu-mint <ryu-mint@ryu-mint.local>
2020-07-23 13:29:19 +0900

    master

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask,render_template
+from flask
+import Flask,render_template,request,dateutil
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html',title="タイトル")
+    msg = request.form.get('msg')
+    return render_template('index.html')
 
 if __name__=='__main__':
     app.debug = True
