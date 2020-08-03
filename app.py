a73da99daa2bfbdd1e7f9993207ceab829a10b77
ryu-mint <ryu-mint@ryu-mint.local>
2020-08-03 14:07:58 +0900

    8.2.database(select)

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask
-import Flask,render_template,request,dateutil
+from flask import Flask,render_template
+import mysql.connector as db
+
+db_param = {
+    'user' : 'mysql',
+    'host' : 'localhost',
+    'password' : '',
+    'database' : 'db1'
+}
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    msg = request.form.get('msg')
-    return render_template('index.html')
+    conn = db.connect(**db_param)
+    cur = conn.cursor()
+    stmt = 'SELECT * FROM books'
+    cur.execute(stmt)
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('index.html', books=rows)
 
 if __name__=='__main__':
     app.debug = True
