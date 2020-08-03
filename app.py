65bfe55453f583d5f2b80641a2d1fa1d9c3dbd98
ryu-mint <ryu-mint@ryu-mint.local>
2020-08-03 14:24:18 +0900

    8.3.database( insert )

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request, redirect
 import mysql.connector as db
 
 db_param = {
@@ -20,6 +20,21 @@ def index():
     cur.close()
     conn.close()
     return render_template('index.html', books=rows)
+
+@app.route('/send',methods=['POST'])
+def send():
+    title = request.form.get('title')
+    price = request.form.get('price')
+    if title =="" or price=="":
+            return redirect('/')
+    conn = db.connect(**db_param)
+    cur = conn.cursor()
+    stmt = 'INSERT INTO books(title, price) VALUES(%s, %s'
+    cur.execute(stmt,(title, int(price)))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return redirect('/')
 
 if __name__=='__main__':
     app.debug = True
